5ac5afaff67ad5c24ee0a277bd1b4d0a623bf28b
Tasneem AbdElhakeem <tasniim.hakiim@gmail.com>
2026-06-28 22:23:24 +0300

    feat(network): implement ApolloClient and product data source DI

---
+++ network/src/main/java/com/tasneem/network/di/NetworkModule.kt
@@ -0,0 +1,41 @@
+package com.tasneem.network.di
+
+import com.apollographql.apollo.ApolloClient
+import com.tasneem.network.datasource.product.ProductRemoteDataSource
+import com.tasneem.network.datasource.product.ProductRemoteDataSourceImpl
+import com.tasneem.network.mapper.ProductMapper
+import com.tasneem.safwa.network.BuildConfig
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideApolloClient(): ApolloClient =
+        ApolloClient.Builder()
+            .serverUrl(BuildConfig.SHOPIFY_ENDPOINT)
+            .addHttpHeader("X-Shopify-Storefront-Access-Token", BuildConfig.STOREFRONT_TOKEN)
+            .build()
+
+    @Provides
+    fun provideProductMapper(): ProductMapper {
+        return ProductMapper()
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataSourceModule {
+
+    @Binds
+    abstract fun bindProductRemoteDatasource(
+        impl: ProductRemoteDataSourceImpl
+    ): ProductRemoteDataSource
+}
