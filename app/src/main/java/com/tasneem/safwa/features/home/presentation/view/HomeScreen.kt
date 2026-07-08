d751a189f186a1a178c9182e3beff7c36af6d0c6
Ashraf Sherif <shifoashraf75@gmail.com>
2026-07-09 01:22:09 +0300

    fix missed width import

--- app/src/main/java/com/tasneem/safwa/features/home/presentation/view/HomeScreen.kt
+++ app/src/main/java/com/tasneem/safwa/features/home/presentation/view/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
